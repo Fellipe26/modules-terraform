dc37e6b16d0bcd3ba0c16238aa59eb07288b779c
Fellipe Benoni <30604774+Fellipe26@users.noreply.github.com>
2021-03-28 17:33:53 -0300

    Update main.tf

--- terraform-azurerm-certificate/main.tf
+++ terraform-azurerm-certificate/main.tf
@@ -5,7 +5,4 @@ resource "azurerm_app_service_certificate_order" "certificate" {
   distinguished_name  = "${var.naked_domain_host_name}"
   product_type        = "${var.certificate_type}"
   validity_in_years   = "${var.validity_in_years}"
-  #provisioner "local-exec" {
-  #  command = "openssl pkcs12 -in key-vault-tst-sices-certificate-tst0ef3ce33-0d7e-4ad1-94dd-90c57020fb7d-20191205.pfx -nocerts -out ct.key"
-  #}  
 }
